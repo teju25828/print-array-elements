8cdbceaec5f19f585acdf792ee06417309e70bfe
Raaj <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>
2022-09-21 19:37:16 +0530

    final commit

---
+++ src/PrintArrayElements.java
@@ -0,0 +1,8 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int array[]={10,20,30,40,50,60};
+        System.out.println("Array elements are : ");
+        for (int i=0;i< array.length;i++)
+            System.out.println(array[i]);
+    }
+}
